5ededbafb8527f76a88a0fb9da80ac7c06961849
salahuddingfx <salauddinkaderappy@gmail.com>
2026-05-09 20:36:04 +0600

    feat: add nullable parent_id foreign key to categories table

--- database/migrations/2026_05_09_143547_add_parent_id_to_categories_table.php
+++ database/migrations/2026_05_09_143547_add_parent_id_to_categories_table.php
@@ -12,17 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade')->after('site_id');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->dropForeign(['parent_id']);
+            $table->dropColumn('parent_id');
         });
     }
 };
