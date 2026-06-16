006ffbd58bd3d8064a40be4380207502fe6c9bd1
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-16 19:13:23 +0600

    feat: configure CORS settings to allow multiple frontend origins

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,11 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => explode(',', env('FRONTEND_URL', 'http://localhost:5173,http://127.0.0.1:5173')),
+    'allowed_origins' => array_filter(array_merge(
+        explode(',', env('ADMIN_URL', 'http://localhost:5173,http://127.0.0.1:5173')),
+        explode(',', env('TAJASHUTKI_URL', 'http://localhost:5174,http://127.0.0.1:5174')),
+        explode(',', env('ACHARU_URL', 'http://localhost:5175,http://127.0.0.1:5175'))
+    )),
 
     'allowed_origins_patterns' => [],
 
